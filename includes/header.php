d444b94c189bb9c18a418806723d33d52a4c6aab
kiranpranay <pranaybannu12@gmail.com>
2023-06-12 14:03:32 +0530

    stm: rebrand to management.stmorg.in
    BringUp users page
    
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,7 @@ if(isset($_SESSION['ID']) && $_SESSION['ID']!=''){
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <title>STM | Medical Camp Management</title>
+    <title>STM Management</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css" />
     <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css" />
@@ -23,7 +23,7 @@ if(isset($_SESSION['ID']) && $_SESSION['ID']!=''){
     <!-- inject:css -->
     <!-- endinject -->
     <!-- ovveride css -->
-    <link rel="stylesheet" href="assets/css/custom_override.css" />
+    <link rel="stylesheet" href="assets/css/custom_override.css?v=1" />
     <!-- Layout styles -->
     <link rel="stylesheet" href="assets/css/style.css" />
 
@@ -36,7 +36,7 @@ if(isset($_SESSION['ID']) && $_SESSION['ID']!=''){
         <!-- partial:partials/_navbar.html -->
         <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
             <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-                <a class="navbar-brand brand-logo" href="index.html">STM Medical Camp</a>
+                <a class="navbar-brand brand-logo" href="index.html">STM Management</a>
                 <a class="navbar-brand brand-logo-mini" href="index.html"><img
                         src="https://stmorg.in/accesories/service_to_man_kind-20200709-0001.jpg" width="28px"
                         height="28px" alt="logo" /></a>
@@ -118,19 +118,37 @@ if(isset($_SESSION['ID']) && $_SESSION['ID']!=''){
                         </a>
                     </li>
                     <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic8" aria-expanded="false"
+                            aria-controls="ui-basic8">
+                            <span class="menu-title">Users</span>
+                            <i class="menu-arrow"></i>
+                            <i class="mdi mdi-comment-plus-outline menu-icon"></i>
+                        </a>
+                        <div class="collapse" id="ui-basic8">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="users">List Users</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="manage_users">Manage Users</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false"
                             aria-controls="ui-basic">
-                            <span class="menu-title">Patients</span>
+                            <span class="menu-title">Coordinators</span>
                             <i class="menu-arrow"></i>
                             <i class="mdi mdi-comment-plus-outline menu-icon"></i>
                         </a>
                         <div class="collapse" id="ui-basic">
                             <ul class="nav flex-column sub-menu">
                                 <li class="nav-item">
-                                    <a class="nav-link" href="verify_patients">Verify Patients</a>
+                                    <a class="nav-link" href="coordinators">Coordinators</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="manage_patients">Manage Patients</a>
+                                    <a class="nav-link" href="manage_coordinators">Manage Coordinators</a>
                                 </li>
                             </ul>
                         </div>
@@ -138,17 +156,17 @@ if(isset($_SESSION['ID']) && $_SESSION['ID']!=''){
                     <li class="nav-item">
                         <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic2" aria-expanded="false"
                             aria-controls="ui-basic2">
-                            <span class="menu-title">Records</span>
+                            <span class="menu-title">Volunteers</span>
                             <i class="menu-arrow"></i>
                             <i class="mdi mdi-comment-plus-outline menu-icon"></i>
                         </a>
                         <div class="collapse" id="ui-basic2">
                             <ul class="nav flex-column sub-menu">
                                 <li class="nav-item">
-                                    <a class="nav-link" href="view_records">View Records</a>
+                                    <a class="nav-link" href="volunteers">Volunteers</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="manage_records">Manage Records</a>
+                                    <a class="nav-link" href="manage_volunteers">Manage Volunteers</a>
                                 </li>
                             </ul>
                         </div>
@@ -156,32 +174,94 @@ if(isset($_SESSION['ID']) && $_SESSION['ID']!=''){
                     <li class="nav-item">
                         <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic3" aria-expanded="false"
                             aria-controls="ui-basic3">
-                            <span class="menu-title">Investigations</span>
+                            <span class="menu-title">Donations</span>
                             <i class="menu-arrow"></i>
                             <i class="mdi mdi-comment-plus-outline menu-icon"></i>
                         </a>
                         <div class="collapse" id="ui-basic3">
                             <ul class="nav flex-column sub-menu">
                                 <li class="nav-item">
-                                    <a class="nav-link" href="blood">Blood</a>
+                                    <a class="nav-link" href="donations">List Donations</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="manage_records">ECG</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="manage_records">X-Ray</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="manage_records">C-T</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="manage_records">MRI</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="manage_records">Ultra Sound</a>
+                                    <a class="nav-link" href="manage_donations">Manage Donations</a>
                                 </li>
                             </ul>
                         </div>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic4" aria-expanded="false"
+                            aria-controls="ui-basic4">
+                            <span class="menu-title">Spends</span>
+                            <i class="menu-arrow"></i>
+                            <i class="mdi mdi-comment-plus-outline menu-icon"></i>
+                        </a>
+                        <div class="collapse" id="ui-basic4">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="spends">List Spends</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="manage_spends">Manage Spends</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic5" aria-expanded="false"
+                            aria-controls="ui-basic5">
+                            <span class="menu-title">Document Registration</span>
+                            <i class="menu-arrow"></i>
+                            <i class="mdi mdi-comment-plus-outline menu-icon"></i>
+                        </a>
+                        <div class="collapse" id="ui-basic5">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="documents">List Documents</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="manage_documents">Manage Documents</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic6" aria-expanded="false"
+                            aria-controls="ui-basic6">
+                            <span class="menu-title">Activities</span>
+                            <i class="menu-arrow"></i>
+                            <i class="mdi mdi-comment-plus-outline menu-icon"></i>
+                        </a>
+                        <div class="collapse" id="ui-basic6">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="activities">List Activities</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="manage_activities">Manage Activities</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic7" aria-expanded="false"
+                            aria-controls="ui-basic7">
+                            <span class="menu-title">Camps</span>
+                            <i class="menu-arrow"></i>
+                            <i class="mdi mdi-comment-plus-outline menu-icon"></i>
+                        </a>
+                        <div class="collapse" id="ui-basic7">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="camps">List Camps</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="manage_camps">Manage Camps</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+
+
                 </ul>
             </nav>
